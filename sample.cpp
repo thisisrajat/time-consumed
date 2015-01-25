5c668a9de6bd05b0c677e733a5ae85f3fbb8fa40
Rajat Jain <rajat.rj10@gmail.com>
2015-01-25 14:39:38 +0530

    sample file for testing

--- sample.cpp
+++ sample.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 
-  for(int i = 1; i <= 100000; i++) {
+  for(int i = 1; i <= 10000000; i++) {
     for(int j = 1; j <= 10; j++) {
       cout << __gcd(i, j) << endl;
     }
